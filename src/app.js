ca288e31ee2c6acdef858dcae1e87282d491d4e9
Nandhu-code94 <Nandhu28041994@gmail.com>
2025-02-03 23:52:12 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("namastw from the dashboard");
-    })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server");
-    })
-
-app.use("/test",(req,res)=>{
-res.send("test from the server");
+// This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Nandhu", lastName:"moorthi"})
 })
 
-app.listen(3000,()=>{
+app.post("/user",(req,res)=>{
+    console.log("Save Data to the database");
+    res.send("Data successfully saved to the database");
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully");
+})
+
+// this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+    res.send("test from the server");
+})
+app.listen(3000, () => {
     console.log("Server is successfully listeninig on port 3000");
 });
